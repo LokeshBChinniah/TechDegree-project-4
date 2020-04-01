5005cf6fde30ba6e8c8f8a548407117297602a9d
LokeshBChinniah <57022188+LokeshBChinniah@users.noreply.github.com>
2020-04-01 12:26:19 -0400

    classes and methods
    added initial classes and methods

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ const game = new Game();
+
+ document.getElementById(btn_reset).addEventListener('click', function() {
+     game.startGame();
+
+    this.style.display = 'none';
+
+ })
+
+ document.getElementsByClassName('key').addEventListener('click', function() {
+     game.handleInteraction();
+
+     
+ })
